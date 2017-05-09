e771035d3d3615ce47a2cb5cc76e9fab54e46f7b
Benjamin Cirmena <benji@benjamins-macbook-pro.home>
2017-05-09 18:12:34 +0200

    Edit city functionalities added

--- mirror/annyang-commands/commands-home.js
+++ mirror/annyang-commands/commands-home.js
@@ -73,10 +73,13 @@ var commands = {
       //check if eyepostition +- 100 of wheater element
       if(xprediction-100 < document.getElementById('weather').getBoundingClientRect().left   && xprediction+100 > document.getElementById('weather').getBoundingClientRect().left && yprediction-100 < document.getElementById('weather').getBoundingClientRect().top   && yprediction+100 > document.getElementById('weather').getBoundingClientRect().top){
         document.getElementById("weather").style.color = 'red';
-        document.getElementById("quote").innerHTML="Wetter wurde Selektiert, um  die Stadt zu ändern sagen Sie zum Beispiel: -Stadt Bern- ";
+        document.getElementById('cityTextArea').value = texts.HOME_INFO_TEXTBOX_CITY;
+        document.getElementById("quote").innerHTML= texts.HOME_INFO_QUOTE_CITY_1;
+        document.getElementById('cityTextArea').style.display = "block";
         selectedId = "weather";
       }else{
         if(timer>=200){
+          document.getElementById('cityTextArea').style.display = "none";
           document.getElementById("weather").style.color = 'white';
           document.getElementById("quote").innerHTML="";
           selectedId = "";
@@ -113,20 +116,65 @@ var commands = {
       document.getElementById('loading').style.display = "none";
     });
   },
+  'what is the weather in *stadt': function(stadt) {
+    /*forecastWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
+    currentWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
+    initWeather();
+    initForecast();*/
+    console.log("wie ist das wetter in"+stadt);
+    document.getElementById('loading').style.display = "block";
+    speechWetterUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
+    $.getJSON(speechWetterUrl, function(data) {
+      var text = 'In ' + data.name + ' its ' +data.main.temp+'°C';
+      textToVoice(text,texts.language);
+      document.getElementById('loading').style.display = "none";
+    }).fail(function() {
+      var text = "Weather data for "+stadt+" could not be loaded";
+      textToVoice(text,language);
+      document.getElementById('loading').style.display = "none";
+    });
+  },
   'stadt *stadt': function(stadt) {
     if(selectedId == "weather"){
+      document.getElementById('cityTextArea').value = stadt;
+      document.getElementById("quote").innerHTML= texts.HOME_INFO_QUOTE_CITY_2;
+    }
+  },
+  'city *stadt': function(stadt) {
+    if(selectedId == "weather"){
+      document.getElementById('cityTextArea').value = stadt;
+      document.getElementById("quote").innerHTML= texts.HOME_INFO_QUOTE_CITY_2;
+    }
+  },
+  'bestätigen': function() {
+    if(selectedId == "weather"){
+      var stadt = document.getElementById('cityTextArea').value;
       forecastWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
       currentWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
+      document.getElementById("quote").innerHTML= "";
+      document.getElementById('cityTextArea').style.display = "none";
+      initWeather();
+      initForecast();
+    }
+  },
+  'save': function() {
+    if(selectedId == "weather"){
+      var stadt = document.getElementById('cityTextArea').value;
+      forecastWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
+      currentWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q='+ stadt +'&units=metric&id=524901&APPID='+weatherApiKey;
+      document.getElementById("quote").innerHTML= "";
+      document.getElementById('cityTextArea').style.display = "none";
       initWeather();
       initForecast();
     }
   },
   'sprache englisch': function() {
+    console.log("changing to english");
     language = "en";
     annyang.setLanguage("en");
     console.log(language);
-    initText();
-    initWeatherText();
+    initText(language);
+    initWeatherText(language);
     initForecast();
     initWeather();
   },
@@ -134,8 +182,8 @@ var commands = {
     language = "de";
     annyang.setLanguage("de");
     console.log(language);
-    initText();
-    initWeatherText();
+    initText(language);
+    initWeatherText(language);
     initForecast();
     initWeather();
   },
